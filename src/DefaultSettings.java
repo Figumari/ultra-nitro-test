c52f36172d6c97d65670505d99451d82ffe58396
Mario Winiker <mario.winiker@gmail.com>
2016-12-19 17:21:21 +0100

    News Files

---
+++ src/DefaultSettings.java
@@ -0,0 +1,12 @@
+/**
+ * Created by mario on 06/12/2016.
+ */
+public interface DefaultSettings {
+
+
+    String getString();
+
+    default void addString(String name) {
+.
+    }
+}
---
+++ src/DefaultTest.java
@@ -0,0 +1,12 @@
+/**
+ * Created by mario on 06/12/2016.
+ */
+public class DefaultTest implements DefaultSettings {
+    private String name;
+
+    public DefaultTest(String name) {
+        this.name = name;
+    }
+
+    public void
+}
